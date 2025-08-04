301e4f149421e8a6d2ed098a9a5e78e78031f475
Igor <IFotler@yandex.ru>
2025-08-04 22:03:21 +0700

    fix checkstyle

--- ewm-service/src/main/java/ru/practicum/EwmService.java
+++ ewm-service/src/main/java/ru/practicum/EwmService.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EwmService
-{
-    public static void main( String[] args ) {
+public class EwmService {
+    public static void main(String[] args) {
         SpringApplication.run(EwmService.class, args);
     }
 }
