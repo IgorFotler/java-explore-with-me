cd480cba43739c8a9334c76e679fdf2addc248be
Igor <IFotler@yandex.ru>
2025-09-02 01:35:38 +0700

    fix checkstyle

--- ewm-service/src/main/java/ru/practicum/comments/mapper/CommentMapper.java
+++ ewm-service/src/main/java/ru/practicum/comments/mapper/CommentMapper.java
@@ -2,7 +2,6 @@ package ru.practicum.comments.mapper;
 
 import org.springframework.stereotype.Component;
 import ru.practicum.comments.dto.CommentDto;
-import ru.practicum.comments.dto.NewCommentDto;
 import ru.practicum.comments.model.Comment;
 import ru.practicum.events.mapper.EventMapper;
 import ru.practicum.users.mapper.UserMapper;
