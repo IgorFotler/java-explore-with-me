e6d4263bcb1b61c03440ee8bebd5848b5d818d78
Igor <IFotler@yandex.ru>
2025-09-02 01:34:03 +0700

    fix checkstyle

--- ewm-service/src/main/java/ru/practicum/comments/service/admin/AdminCommentService.java
+++ ewm-service/src/main/java/ru/practicum/comments/service/admin/AdminCommentService.java
@@ -1,7 +1,6 @@
 package ru.practicum.comments.service.admin;
 
 import ru.practicum.comments.dto.CommentDto;
-import ru.practicum.comments.model.CommentStatus;
 
 public interface AdminCommentService {
 
